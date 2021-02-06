e7b255b8193380844ece16466159a2e48987eae6
Pedro Lopes <pfmiralopes@gmail.com>
2021-02-06 22:53:01 +0000

    add base for netlify functions

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,13 +1,26 @@
-
+import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 const Home = () => {
   const { t } = useTranslation('home')
 
+  const [response, setResponse] = useState('nothing yet')
+
+  const onClick = () => {
+    fetch("/.netlify/functions/hello")
+      .then(response => response.json())
+      .then(json => setResponse(json.msg))
+  }
+
   return <section>
     <h1>
       {t('title')}
     </h1>
+    <p>
+      {response}
+    </p>
+
+    <button type='button' onClick={onClick}>Call lambda</button>
   </section>
 }
 
